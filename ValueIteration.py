c43a7b00b38bdf07039f88b36125477e62579848
LXXHSI007 <lxxhsi007@myuct.ac.za>
2021-05-25 16:00:15 +0200

    ValueIteration.py : Started and working on setting random mines, will revise it later

--- ValueIteration.py
+++ ValueIteration.py
@@ -28,17 +28,32 @@ def main(argv):
     height = int(argv[1])
     endpos = [height - 1, height - 1]
     while count < len(argv):
-       if argv[count] == "-start":
+        if argv[count] == "-start":
            startpos = [int(argv[count + 1]), int(argv[count + 2])]
            count += 1
-       elif argv[count] == "-end":
+        elif argv[count] == "-end":
            endpos = [int(argv[count + 1]), int(argv[count + 2])]
            count += 1
-       elif argv[count] == "-gamma":
+        elif argv[count] == "-gamma":
            gamma = float(argv[count + 1])
-       elif argv[count] == "-k":
+        elif argv[count] == "-k":
            k = int(argv[count + 1])
-       count += 1
+        count += 1
+
+    #set mines
+    if k > 0:
+        r_height = [*range(0, height)]
+        r_width = [*range(0, width)]
+
+        if startpos[0] in r_width:
+            r_width.remove(startpos[0])
+        if endpos[1] in r_height:
+            r_height.remove(endpos[1])
+        for i in range(0,k):
+            temp_x = random.choice(r_width)
+            temp_y = random.choice(r_height)
+            mines.append([temp_x,temp_y])
+
 
 
 if __name__ == '__main__':
